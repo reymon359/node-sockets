4b52b11b7a838e06e62fb8d07fb2104e73762de7
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-22 10:36:04 +0100

    Installing and configuring soclet.io

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const path = require('path');
+
+const app = express();
+// Now we define the server where the app will run
+let server = http.createServer(app);
+
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+// IO is the backend communication  
+let io = socketIO(server);
+
+server.listen(port, (err) => {
+
+    if (err) throw new Error(err);
+
+    console.log(`Server running on port ${ port }`);
+
+});
